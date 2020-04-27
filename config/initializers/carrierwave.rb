207303a8878e1fc2ef7bbc3a355248e7f33cfe2f
SuperLap <SuperLap@SuperLaps-MacBook-Pro.local>
2020-04-27 12:58:05 +0700

    imahge

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ if ENV["AWS_BUCKET"] &&
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],                        # required
-    :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"]   
+    :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],   
     :aws_signature_version => 4                     # required
   }
   config.fog_directory  = ENV["AWS_BUCKET"]                     # required
